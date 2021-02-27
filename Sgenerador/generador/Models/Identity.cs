dc7f6c9bd7015b545f7eababe244b1be8cb967de
JoseGabrielZeballos <a2019117789@estudiantes.upsa.edu.bo>
2021-02-27 12:05:39 -0400

    Ejercicio completo.

---
+++ Sgenerador/generador/Models/Identity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace generador.Models
+{
+    public class Identity
+    {
+        public int IdentityId { get; set; }
+        public string Name { get; set; }
+        public string Photo { get; set; }
+        public string Sex { get; set; }
+
+    }
+}
